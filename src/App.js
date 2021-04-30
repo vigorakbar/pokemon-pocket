d83da11d8c6f778bfbd55f0a1ba62baf65c61af6
Vigor Akbar <vigorakbar@gmail.com>
2021-04-30 22:00:17 +0700

    setup page routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import { Container } from "@material-ui/core";
+import { Route, Switch } from "react-router";
 import DefaultAppBar from "./components/AppBar/DefaultAppBar";
+import MyPokemon from "./components/pages/MyPokemon/MyPokemon";
+import Pokedex from "./components/pages/Pokedex/Pokedex";
+import PokemonDetail from "./components/pages/PokemonDetail/PokemonDetail";
 
 function App() {
   return (
@@ -10,6 +15,23 @@ function App() {
       `}
     >
       <DefaultAppBar />
+      <Container
+        css={css`
+          margin-top: 72px;
+        `}
+      >
+        <Switch>
+          <Route exact path="/">
+            <Pokedex />
+          </Route>
+          <Route path="/my-pokemon">
+            <MyPokemon />
+          </Route>
+          <Route path="/my-pokemon/:id">
+            <PokemonDetail />
+          </Route>
+        </Switch>
+      </Container>
     </div>
   );
 }
