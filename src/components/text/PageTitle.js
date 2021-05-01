8357112ef6565ae835ea949779f89184fe1f6075
Vigor Akbar <vigorakbar@gmail.com>
2021-05-01 22:59:06 +0700

    adjust page title style

--- src/components/text/PageTitle.js
+++ src/components/text/PageTitle.js
@@ -1,17 +1,22 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import { Typography } from "@material-ui/core";
+import { Typography, useTheme } from "@material-ui/core";
 import palettes from "../../const/palettes";
 
-const cssTitle = css`
+const cssTitle = (theme) => css`
   color: ${palettes.colors.black};
   font-weight: 600;
   margin-top: 32px;
+
+  ${theme.breakpoints.up("sm")} {
+    text-align: center;
+  }
 `;
 
 const PageTitle = ({ children, ...otherProps }) => {
+  const theme = useTheme();
   return (
-    <Typography variant="h4" css={cssTitle} {...otherProps}>
+    <Typography variant="h4" css={cssTitle(theme)} {...otherProps}>
       {children}
     </Typography>
   );
